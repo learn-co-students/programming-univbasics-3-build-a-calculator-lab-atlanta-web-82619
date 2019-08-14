f324742a985d8deb45a5bf45f377e7ccd5cc7cc8
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-14 17:29:02 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-
+  add = num1 + num2
+  return add
 end
 
 def subtraction(num1, num2)
-
+  subtract = num1 - num2
+  return subtract
 end
 
 def division(num1, num2)
-
+  divide = num1 / num2
+  return divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  return multiply
 end
 
 def modulo(num1, num2)
-
+  mod = num1 %= num2
+  return mod
 end
 
 def square_root(num)
-
+  return Math.sqrt(num)
 end
